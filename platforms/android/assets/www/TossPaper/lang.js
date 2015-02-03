aff796243564cec1962521f0d5f401a2423c8b16
lizhijumacc <zhi.li@jumacc.com>
2015-02-04 00:09:22 +0800

    add paper toss game.

---
+++ platforms/android/assets/www/TossPaper/lang.js
@@ -0,0 +1,137 @@
+window.languages = {
+		
+	"en": {
+		"startGame": "START GAME",
+		"selectLevel": "SELECT LEVEL",
+		"easy": "EASY",
+		"medium": "MEDIUM",
+		"hard": "HARD",
+		"youNeed": "YOU NEED",
+		"youNeed2": "POINTS",
+		"points": "POINTS",
+		"best": "Best",
+		"resume": "RESUME",
+		"backToMenu": "BACK TO MENU",
+		"moreGames": "More games"
+	},
+	
+	"de": {
+		"startGame": "SPIEL STARTEN",
+		"selectLevel": "LEVEL WÄHLEN",
+		"easy": "EINFACH",
+		"medium": "MITTEL",
+		"hard": "SCHWER",
+		"youNeed": "DU BRAUCHST",
+		"youNeed2": "Punkte",
+		"points": "PUNKTE",
+		"best": "Beste",
+		"resume": "FORTFAHREN",
+		"backToMenu": "ZURÜCK ZUM MENÜ",
+		"moreGames": "Weitere Spiele"
+	},
+	
+	"es": {
+		"startGame": "COMENZAR EL JUEGO",
+		"selectLevel": "SELECCIONAR NIVEL",
+		"easy": "FACIL",
+		"medium": "MEDIO",
+		"hard": "DIFICIL",
+		"youNeed": "NECESITAS",
+		"youNeed2": "Puntos",
+		"points": "PUNTOS",
+		"best": "Best",
+		"resume": "CONTINUAR",
+		"backToMenu": "VOLVER AL MENU",
+		"moreGames": "MAS JUEGOS"
+	},
+	
+	"fr": {
+		"startGame": "DÉMARRER LE JEU",
+		"selectLevel": "NIVEAU SELECT",
+		"easy": "FACILE",
+		"medium": "MOYEN",
+		"hard": "DUR",
+		"youNeed": "VOUS AVEZ BESOIN",
+		"youNeed2": "Points",
+		"points": "POINTS",
+		"best": "meilleur",
+		"resume": "RESUME",
+		"backToMenu": "RETOUR AU MENU",
+		"moreGames": "Plus de jeux"
+	},
+	
+	"it": {
+		"startGame": "INIZIO PARTITA",
+		"selectLevel": "SELEZIONARE IL LIVELLO",
+		"easy": "EASY",
+		"medium": "MEDIUM",
+		"hard": "DURO",
+		"youNeed": "HAI BISOGNO",
+		"youNeed2": "punti",
+		"points": "PUNTI",
+		"best": "migliore",
+		"resume": "RIPRESA",
+		"backToMenu": "TORNA AL MENU",
+		"moreGames": "Altri giochi"
+	},
+	
+	"tr": {
+		"startGame": "START GAME",
+		"selectLevel": "SEÇ SEVİYE",
+		"easy": "KOLAY",
+		"medium": "ORTA",
+		"hard": "HARD",
+		"youNeed": "İHTİYACINIZ",
+		"youNeed2": "makas",
+		"points": "MAKAS",
+		"best": "en iyi",
+		"resume": "RESUME",
+		"backToMenu": "Menüye geri dön",
+		"moreGames": "Daha fazla oyun"
+	},
+	
+	"pt": {
+		"startGame": "INICIAR O JOGO",
+		"selectLevel": "SELECIONE O NIVEL",
+		"easy": "EASY",
+		"medium": "MÉDIO",
+		"hard": "DURO",
+		"youNeed": "VOCÊ PRECISA",
+		"youNeed2": "pontos",
+		"points": "PONTOS",
+		"best": "o melhor",
+		"resume": "RESUME",
+		"backToMenu": "Voltar ao MENU",
+		"moreGames": "Mais jogos"
+	},
+	
+	"ru": {
+		"startGame": "НАЧНИ ИГРУ",
+		"selectLevel": "ВЫБЕРИ УРОВЕНЬ",
+		"easy": "ПРОСТОЙ",
+		"medium": "СРЕДНИЙ",
+		"hard": "СЛОЖНЫЙ",
+		"youNeed": "ТЕБЕ НУЖНО",
+		"youNeed2": "Очки",
+		"points": "ОЧКИ",
+		"best": "Топ",
+		"resume": "ПРОДОЛЖИТЬ",
+		"backToMenu": "ВЕРНУТЬСЯ К МЕНЮ",
+		"moreGames": "Больше игр"
+	},
+
+	"pl": {
+		"startGame": "ZACZNIJ GRĘ",
+		"selectLevel": "WYBIERZ POZIOM",
+		"easy": "ŁATWY",
+		"medium": "ŚREDNI",
+		"hard": "TRUDNY",
+		"youNeed": "POTRZEBUJESZ CO NAJMNIEJ",
+		"youNeed2": "PUNKTÓW",
+		"points": "Punkty",
+		"best": "Rekord",
+		"resume": "WZNÓW",
+		"backToMenu": "WRÓĆ DO MENU",
+		"moreGames": "WIÄ˜CEJ GIER"
+	}
+};
